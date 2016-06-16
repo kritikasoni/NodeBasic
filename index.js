d1c24669eb079eb36d97eb09fff44dbfd3821b80
kris <kritika_soni@cmu.ac.th>
2016-06-16 15:17:05 +0700

    learn to print primitive data types

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 'use strict';
 console.log('Hello World!');
-const day='Tuesday'//const คือห้ามเขียนทับ ได้ครั้งเดียว
+//const day='Tuesday'//const คือห้ามเขียนทับ ได้ครั้งเดียว
 let day='Monday';
 console.log(day);
+let a = null;
+console.log(typeof 'Hello');
+console.log(typeof 1);
+console.log(typeof 0);
+console.log(typeof true);
+console.log(typeof undefined);
+console.log(typeof null);
+console.log('guess','-',a);
