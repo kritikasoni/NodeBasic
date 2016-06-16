10ae39a5e97096847cfcf7f1e1d472b92662e92f
kris <kritika_soni@cmu.ac.th>
2016-06-16 17:10:58 +0700

    learn some

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 'use strict';
-console.log('Hello World!');
+//console.log('Hello World!');
 //const day='Tuesday'//const คือห้ามเขียนทับ ได้ครั้งเดียว
-let day='Monday';
-console.log(day);
+
 const student1 ={
     id:'562115003',
-    name:'Kris'
+    name:'Kris',
+    grade: 4.00
 };
 const student2 ={
     id:'562115028',
-    name:'Nurse'
+    name:'Nurse',
+    grade: 5.0
 };
 const student3 ={
     id:'562115012',
-    name:'EJ'
+    name:'EJ',
+    grade: 4.5
 };
 
 const students=[student1,student2,student3];
@@ -26,4 +28,8 @@ const getStudentById= (id) =>{
         students.filter(student => student.id == id).map(student=>'This is student number:'+student.id).pop() //map คือการเปลี่ยนรูป
     );
 };
+const isSomeStudentGradeGreaterThan2= () =>{
+    return students.some(student=>student.grade>2.0);  //มี นร อย่างน้อย 1 คนที่ได้เกรดมากกว่า  2 หรือไม่
+};
 getStudentById('562115003')
+console.log(isSomeStudentGradeGreaterThan2());
