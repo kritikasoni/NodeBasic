84e1fa39558c56bcf59307a70b6f8833f643311a
kris <kritika_soni@cmu.ac.th>
2016-06-16 15:27:09 +0700

    learn to create array

--- index.js
+++ index.js
@@ -3,8 +3,13 @@ console.log('Hello World!');
 //const day='Tuesday'//const คือห้ามเขียนทับ ได้ครั้งเดียว
 let day='Monday';
 console.log(day);
-const student ={
+const student1 ={
     id:'562115003',
     name:'Kris'
-}
-console.log(student);
+};
+const student2 ={
+    id:'562115028',
+    name:'Nurse'
+};
+const students=[student1,student2];
+console.log(students);
