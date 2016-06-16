df42cf6ed3d192e6ded87eec8b34895cbb3b83a2
kris <kritika_soni@cmu.ac.th>
2016-06-16 15:22:16 +0700

    learn to create object student

--- index.js
+++ index.js
@@ -3,11 +3,8 @@ console.log('Hello World!');
 //const day='Tuesday'//const คือห้ามเขียนทับ ได้ครั้งเดียว
 let day='Monday';
 console.log(day);
-let a = null;
-console.log(typeof 'Hello');
-console.log(typeof 1);
-console.log(typeof 0);
-console.log(typeof true);
-console.log(typeof undefined);
-console.log(typeof null);
-console.log('guess','-',a);
+const student ={
+    id:'562115003',
+    name:'Kris'
+}
+console.log(student);
